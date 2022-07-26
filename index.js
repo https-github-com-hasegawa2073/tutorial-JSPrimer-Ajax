aacd6cdc9998e7beef9c112a51967f8af2901853
hasegawa2073 <hasetatsu2001@icloud.com>
2022-07-26 21:35:51 +0900

    Add: レスポンスの受け取り

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('index.js: loaded');
+const userId = 'hasegawa2073';
+fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`).then(
+  (response) => {
+    console.log(response.status);
+    return response.json().then((userInfo) => {
+      // JSONパースされたオブジェクトが渡される
+      console.log(userInfo);
+    });
+  }
+);
