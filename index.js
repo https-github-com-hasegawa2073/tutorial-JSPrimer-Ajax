84e88e41443c569ab94d93e74021a681b5a364ae
hasegawa2073 <hasetatsu2001@icloud.com>
2022-07-27 12:09:33 +0900

    Improve: HTMLの組み立てと表示の処理を分離した

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
-const userId = 'hasegawa2073';
+async function main() {
+  try {
+    const userId = getUserId();
+    const userInfo = await fetchUserInfo(userId);
+    const view = createView(userInfo);
+    displayView(view);
+  } catch (error) {
+    console.log(`エラーが発生しました ${error}`);
+  }
+}
 
 function fetchUserInfo(userId) {
-  fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+  return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
     .then((response) => {
-      console.log(response.status);
       if (!response.ok) {
-        console.error('エラーレスポンス', response);
+        return Promise.reject(
+          new Error(`${response.status}: ${response.statusText}`)
+        );
       } else {
-        return response.json().then((userInfo) => {
-          // JSONパースされたオブジェクトが渡される
-          console.log(userInfo);
-          const view = `
-            <h4>${userInfo.name} (@${userInfo.login})</h4>
-            <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-            <dl>
-              <dt>Location</dt>
-              <dd>${userInfo.location}</dd>
-              <dt>Repositories</dt>
-              <dd>${userInfo.public_repos}</dd>
-            </dd>
-            `;
-          const result = document.getElementById('result');
-          result.innerHTML = view;
-        });
+        // JSONオブジェクトで解決されるPromiseを返す
+        return response.json();
       }
     })
     .catch((error) => {
       console.error(error);
     });
+}
+
+function getUserId() {
+  return document.getElementById('userId').value;
+}
+
+function createView(userInfo) {
+  return escapeHTML`
+    <h4>${userInfo.name} (@${userInfo.login})</h4>
+    <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+    <dl>
+      <dt>Location</dt>
+      <dd>${userInfo.location}</dd>
+      <dt>Repositories</dt>
+      <dd>${userInfo.public_repos}</dd>
+    </dl>
+    `;
+}
+
+function displayView(view) {
+  const result = document.getElementById('result');
+  result.innerHTML = view;
 }
 
 function escapeSpecialChars(str) {
